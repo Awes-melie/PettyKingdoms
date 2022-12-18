f861b8d3f124a81703a105f36bc22b618ce1798d
Btdyaya <amelietdavies@gmail.com>
2022-12-18 15:46:37 +0000

    Save system working

--- Interface.java
+++ Interface.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+//import java.net.SocketImpl;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -19,8 +19,8 @@ public class Interface {
 
     Boolean gameActive = true;
 
-    onStart(world);
-    
+    world = onStart(world);
+
     while (gameActive){
         String command = input.readLine();
         String[] operations = command.split(" ");
@@ -32,21 +32,30 @@ public class Interface {
                 saveWorld(world);
             }
         } else {
-            System.out.println(String.format("... my %s?",world.getTitle()));
+            puts("... my %s?",world);
         }
     }
-    System.out.println(String.format("Goodbye my %s",world.getTitle()));
+    puts("Goodbye my %s?",world);
     }
 
     public static WorldState loadWorld(){
         try {
+
             FileInputStream file = new FileInputStream(new File("file.txt"));
             return WorldState.load(file);
+
         } catch(FileNotFoundException e) {
+
             System.out.println("File not found");
             return new WorldState();
+
         }
     }
+
+    public static void puts(String text, WorldState world){
+        System.out.println(String.format(text, world.getTitle()));
+    }
+
     public static void saveWorld(WorldState world){
         try {
             FileOutputStream file = new FileOutputStream(new File("file.txt"));
@@ -56,40 +65,60 @@ public class Interface {
         }
     }
 
-    public static void onStart(WorldState world)
-        throws IOException
+    public static WorldState onStart(WorldState world)
+        throws IOException 
     {
         System.out.println("Do you have a realm already? y|n");
         Boolean inputInvalid = true;
+
         while (inputInvalid){
+
             String command = input.readLine();
 
             if(command.toUpperCase().compareTo("Y") == 0) {
+
                 world = loadWorld();
-                return;
+                return world;
+
             } else if (command.toUpperCase().compareTo("N") == 0){
-                world = WorldState.generateWorld();
-                System.out.println("Are you a lord or a lady my leige? lord|lady|neither");
+
+                world = WorldState.generateNewWorld();
+                System.out.println("Are you a lord or a lady? lord|lady|neither");
                 inputInvalid = false;
+
             } else {
+
                 System.out.println("... My leige? Do you have a realm already? y|n");
+
             }
         }
+
         inputInvalid = true;
+
         while (inputInvalid){
+
             String command = input.readLine();
             if(command.toUpperCase().compareTo("LORD") == 0) {
+
                 world.setTitle("Lord");
-                return;
+                inputInvalid = false;
+
             } else if (command.toUpperCase().compareTo("LADY") == 0){
+
                 world.setTitle("Lady");
-                return;
+                inputInvalid = false;
+
             } else if (command.toUpperCase().compareTo("NEITHER") == 0){
+
                 world.setTitle("Leige");
-                return;
+                inputInvalid = false;
+
             } else {
-                System.out.println("... My leige? Are you a lord or a lady my leige? lord|lady|neither");
+
+                System.out.println("... Are you a lord or a lady? lord|lady|neither");
+
             }
         }
+        return world;
     }
 }
